171752c1786d59920d6f12fce860da677d94e350
Mark Evangelista <mark172@me.com>
2016-05-10 04:13:20 +0000

    added server side validations

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,2 +1,5 @@
 class Quote < ActiveRecord::Base
+    validates :first_name, :last_name, :email, :phone, :pick_up_date, :origin_city, :origin_state, :origin_zip, :destination_city, :destination_state, :destination_zip, :year, :make, :model, :is_operable,  presence: true
+    validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+    validates :phone, numericality: true
 end

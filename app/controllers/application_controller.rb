61769ec7024af69de325dcc1d1309cb46f4398e9
Mark Evangelista <mark172@me.com>
2016-05-27 19:24:06 +0000

    added translation for mobie

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  
+  helper_method :mobile?
+
+   private
+
+   def mobile? # has to be in here because it has access to "request"
+      request.user_agent =~ /\b(Android|iPhone|iPad|Windows Phone|Opera Mobi|Kindle|BackBerry|PlayBook)\b/i
+   end
+   
   protect_from_forgery with: :exception
 end
